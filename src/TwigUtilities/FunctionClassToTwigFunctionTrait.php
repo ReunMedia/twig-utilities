e5943a3f419dacd6e1e76117bfad44cd7c1eb333
Kimmo Salmela <cryszon@gmail.com>
2018-04-24 12:30:13 +0300

    Reformat code

--- src/TwigUtilities/FunctionClassToTwigFunctionTrait.php
+++ src/TwigUtilities/FunctionClassToTwigFunctionTrait.php
@@ -2,13 +2,13 @@
 
 namespace Reun\TwigUtilities;
 
-use \InvalidArgumentException,
-    \Twig_Filter,
-    \Twig_Function;
+use \InvalidArgumentException;
+use \Twig_Filter;
+use \Twig_Function;
 
 /**
  * Trait to convert AbstractTwigFunctionClass classes to Twig functions.
- * 
+ *
  * @author Kimmo Salmela <kimmo.salmela@reun.eu>
  */
 trait FunctionClassToTwigFunctionTrait
@@ -16,45 +16,50 @@ trait FunctionClassToTwigFunctionTrait
 
   /**
    * Converts an AbstractTwigFunctionClass to Twig function or filter.
-   * 
+   *
    * @param string $classname Function class name.
    * @param string $type Function type. Either Twig_Function or Twig_Filter.
    * @return Twig_Function|Twig_Filter
    * @throws InvalidArgumentException
    */
-  public function functionClassToTwigFunction(string $classname, string $type)
-  {
-    if($type !== Twig_Function::class && $type !== Twig_Filter::class) {
-      $msg = sprintf("'\$type' must be either '%s' or '%s'",
-        Twig_Function::class, Twig_Filter::class);
-      throw new InvalidArgumentException($msg);
+    public function functionClassToTwigFunction(string $classname, string $type)
+    {
+        if ($type !== Twig_Function::class && $type !== Twig_Filter::class) {
+            $msg = sprintf(
+          "'\$type' must be either '%s' or '%s'",
+        Twig_Function::class,
+          Twig_Filter::class
+      );
+            throw new InvalidArgumentException($msg);
+        }
+
+        if (!is_subclass_of($classname, AbstractTwigFunctionClass::class)) {
+            $msg = sprintf(
+          "%s is not an instance of %s",
+          $classname,
+        AbstractTwigFunctionClass::class
+      );
+            throw new InvalidArgumentException($msg);
+        }
+
+        if (!method_exists($classname, "__invoke")) {
+            $msg = sprintf("%s doesn't implement '__invoke()'", $classname);
+            throw new InvalidArgumentException($msg);
+        }
+
+        $options = $classname::getOptions();
+        $name = $classname::getName();
+
+        $function = new $type($name, [$classname, "__invoke"], $options);
+        return $function;
     }
 
-    if(!is_subclass_of($classname, AbstractTwigFunctionClass::class)) {
-      $msg = sprintf("%s is not an instance of %s", $classname,
-        AbstractTwigFunctionClass::class);
-      throw new InvalidArgumentException($msg);
+    public function functionClassesToTwigFunctions(array $classes, string $type)
+    {
+        $f = [];
+        foreach ($classes as $classname) {
+            $f[] = $this->functionClassToTwigFunction($classname, $type);
+        }
+        return $f;
     }
-
-    if(!method_exists($classname, "__invoke")) {
-      $msg = sprintf("%s doesn't implement '__invoke()'", $classname);
-      throw new InvalidArgumentException($msg);
-    }
-
-    $options = $classname::getOptions();
-    $name = $classname::getName();
-
-    $function = new $type($name, [$classname, "__invoke"], $options);
-    return $function;
-  }
-
-  public function functionClassesToTwigFunctions(Array $classes, string $type)
-  {
-    $f = [];
-    foreach($classes as $classname) {
-      $f[] = $this->functionClassToTwigFunction($classname, $type);
-    }
-    return $f;
-  }
-
 }
