8cd57d624ecba5a69283e80aa5dff25cfd969639
Kimmo Salmela <cryszon@gmail.com>
2018-04-24 13:01:49 +0300

    Add Strftime Filter

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,8 @@
 # Changelog
 
+## v0.2.0 - 2018-04-24
+### New features
+- Add `strftime` filter.
+
 ## v0.1.0 - 2018-04-24
 Initial release.
---
+++ src/TwigUtilities/Filters/Strftime.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Reun\TwigUtilities\Filters;
+
+use \DateTime;
+use \Reun\TwigUtilities\TwigFunctionClass;
+
+/**
+ * Description of Strftime
+ *
+ * @author Kimmo Salmela <kimmo.salmela@reun.eu>
+ */
+class Strftime extends TwigFunctionClass
+{
+    public function __invoke($date, $format)
+    {
+        // FIXME - Workaround for %e on Windows. Taken from http://goo.gl/92vR
+        // Check for Windows to find and replace the %e modifier correctly
+        if (strtoupper(substr(PHP_OS, 0, 3)) == 'WIN') {
+            $format = preg_replace('#(?<!%)((?:%%)*)%e#', '\1%#d', $format);
+            // Leading zero padding fix from unix format (%-m) to Windows format
+            // (%#m)
+            $format = preg_replace("/(?<!%)((?:%%)*)%-/", '\1%#', $format);
+        }
+
+        $timestamp = ($date instanceof DateTime) ? $date->getTimestamp() : strtotime($date);
+
+        // FIXME - Another Windows workaround
+        $formatted = strftime($format, $timestamp);
+        if (strtoupper(substr(PHP_OS, 0, 3)) == 'WIN') {
+            $formatted = utf8_encode($formatted);
+        }
+        return $formatted;
+    }
+}
