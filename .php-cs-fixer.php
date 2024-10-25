4ddedb05c9fd059fbffc1af7baf1cc3eb71b8614
Kimmo Salmela <kimmo.salmela@reun.eu>
2024-10-25 17:26:46 +0300

    Update PHP CS Fixer config

---
+++ .php-cs-fixer.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Reun Media PHP CS Fixer configuration file.
+ *
+ * @author Reun Media <company@reun.eu>
+ * @copyright 2020-2024 Reun Media
+ *
+ * @see https://github.com/Reun-Media/php-app-template/blob/master/app/.php-cs-fixer.php
+ *
+ * @version 3.0.2
+ */
+
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+
+$config = new Config();
+
+return $config
+    ->setRiskyAllowed(true)
+    ->setRules([
+        "@PER-CS2.0" => true,
+        "@PhpCsFixer" => true,
+
+        "declare_strict_types" => true,
+
+        // Prefer double quotes
+        "single_quote" => false,
+
+        // Allows us to use /** @disregard */
+        "phpdoc_to_comment" => false,
+
+        // These two rules allow us to use `#region` comments for region
+        // folding
+        "single_line_comment_spacing" => false,
+        "single_line_comment_style" => [
+            "comment_types" => [
+                'asterisk',
+            ],
+        ],
+    ])
+;
